299cde52c77a0928aedf4306c9fde6fe9a31c80f
hrom897 <hrom.m.cloud.py@gmail.com>
2025-02-07 20:17:02 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7585427991:AAENGj1uCtdUIJhVAwbjRsSWiB4uyiHg7zA")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Користувач зараз: Працює 🥰 \nПерерва в: 13:00 💌")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
